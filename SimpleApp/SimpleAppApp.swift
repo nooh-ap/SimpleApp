4b8b0c9bb68bdfc8954081e3849388449872e910
Nooh AlMahfoodhi <nooh.almahfoodhi@gmail.com>
2022-10-16 00:29:29 -0700

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Nooh AlMahfoodi on 10/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
